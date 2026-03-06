c1eb589e7149adcbb6f245f879fdb721789cb32f
lindkolby <lin21013@byui.edu>
2026-03-05 20:56:55 -0700

    Learning05 completed

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Red", 3);
+        shapes.Add(s1);
+
+        Rectangle r1 = new Rectangle("Blue", 4, 5);
+        shapes.Add(r1); 
+
+        Circle c1 = new Circle("Green", 6);
+        shapes.Add(c1); 
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}"); 
+        }
     }
 }
